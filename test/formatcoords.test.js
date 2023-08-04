49fe3c6ca04acf2ef7c8c31d9c1a776fba7c844b
Niko Ivan <nixonivan@rambler.ru>
2023-08-04 15:00:49 +0300

    test updated

--- test/formatcoords.test.js
+++ test/formatcoords.test.js
@@ -1,6 +1,6 @@
 import formatCoords from "../src/js/components/formatcoords";
 
-const geoExpList = [
+/*const geoExpList = [
   ["[51.50851, -0.12572]", "51.50851, -0.12572"],
   ["51.50de851, -0.12572", null],
   ["51.50851,    -0.12572", "51.50851, -0.12572"],
@@ -11,9 +11,9 @@ const testGeoList = test.each(geoExpList);
 
 testGeoList("Test for method formatCoords by %q", (value, result) => {
   expect(formatCoords(value)).toBe(result);
-});
+});*/
 
-/*test("Test for correct value in method formatCoords", () => {
+test("Test for correct value in method formatCoords", () => {
   const result = formatCoords("[51.50851, -0.12572]");
   expect(result).toBe("51.50851, -0.12572");
 });
@@ -21,4 +21,4 @@ testGeoList("Test for method formatCoords by %q", (value, result) => {
 test("Test for incorrect value in method formatCoords", () => {
   const result = formatCoords("[51.5d0851, -0.12572]");
   expect(result).toBe(null);
-});*/
+});
